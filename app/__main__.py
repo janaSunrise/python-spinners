7d118611448b8dc4493747f90f6c6d922b6ef393
janaSunrise <warriordefenderz@gmail.com>
2021-02-28 20:22:49 +0530

    Complete the refactor.

--- app/__main__.py
+++ app/__main__.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     spinner = random.choice(list(SPINNERS_DICT.keys()))
 
     name = spinner
-    frames = SPINNERS_DICT["frames"]
-    interval = SPINNERS_DICT["interval"]
+    frames = SPINNERS_DICT[name]["frames"]
+    interval = SPINNERS_DICT[name]["interval"]
 
     print_spinner_frames(frames, interval, name)
     print('\n')
--- app/config.py
+++ app/config.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 CLEAR_LINE = "\033[K"
 
-SPINNERS_DICT = json.loads((Path.cwd() / "app" / "assets" / "spinners.json").read_text())["spinners"]
+SPINNERS_DICT = json.loads((Path.cwd() / "app" / "assets" / "spinners.json").read_text())
